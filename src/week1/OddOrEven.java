d8dcba2c6abfba9939616a9a212555c768bd8420
sucharita <chakraborty892@gmail.com>
2023-05-24 18:21:57 -0400

    sucharita's oddOreven

--- src/week1/OddOrEven.java
+++ src/week1/OddOrEven.java
@@ -1,6 +1,19 @@
 package week1;
 
 public class OddOrEven {
+    public static void OddOrEven(int num){
+        if(num % 2==0){
+            System.out.println(num + " is even number");
+        }else{
+            System.out.println(num + " is odd number");
+        }
+    }
+
+    public static void main(String[] args) {
+        OddOrEven(99);
+
+        OddOrEven(-10);
+    }
 
 }
 
