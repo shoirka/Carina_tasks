525476fe75c1b615a2f2375de54aead6f6f1bc55
aaronqa <aaron@cybertekschool.com>
2023-05-25 18:08:26 +0300

    second week questions

--- src/aaron/NumbersDivisibleBy_3_5_15.java
+++ src/aaron/NumbersDivisibleBy_3_5_15.java
@@ -1,7 +1,7 @@
 package aaron;
 
 /**
- *  Numbers -- Divisible by 3, 5, 15
+ * Numbers -- Divisible by 3, 5, 15
  * Write a program that can print the numbers between 1 ~ 100 that can be divisible by 3, 5, and 15.
  * if the number can be divisible by 3, 5 and 15, then it should only be displayed in DivisibelBy15 section
  * if the number can be divisible by 3 but cannot be divisible by 15, then it should only be displayed in DivisibelBy3 section
@@ -13,4 +13,28 @@ package aaron;
  * Divisible By 3 3 6 9 12 18 21 24 27 33 36 39 42 48 51 54 57 63 66 69 72 78 81 84 87 93 96 99
  */
 public class NumbersDivisibleBy_3_5_15 {
+    public static void main(String[] args) {
+        solution(17);
+    }
+
+    public static void solution(int n) {
+        String divisibleBy15 = "";
+        String divisibleBy5 = "";
+        String divisibleBy3 = "";
+        int[] arr = new int[100];
+        for (int i = 0; i < arr.length; i++)
+            arr[i] = i + 1;
+
+        for (int each : arr) {
+            if (each % 15 == 0 && each % 3 == 0)
+                divisibleBy15 += each + " ";
+            if (each % 5 == 0 && each % 15 != 0)
+                divisibleBy5 += each + " ";
+            if (each % 3 == 0 && each % 15 != 0)
+                divisibleBy3 += each + " ";
+        }
+        System.out.println("Divisible By 15: " + divisibleBy15);
+        System.out.println("Divisible By 5: " + divisibleBy5);
+        System.out.println("Divisible By 3: " + divisibleBy3);
+    }
 }
