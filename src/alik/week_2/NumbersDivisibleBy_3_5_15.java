659039c5a43a1ede9596b8842cf00c0126b27db2
Aliy Shegidevich <shegidevich@gmail.com>
2023-05-31 22:07:51 -0400

    Week 2 solutions

--- src/alik/week_2/NumbersDivisibleBy_3_5_15.java
+++ src/alik/week_2/NumbersDivisibleBy_3_5_15.java
@@ -1,7 +1,12 @@
 package alik.week_2;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+
 /**
- *  Numbers -- Divisible by 3, 5, 15
+ * Numbers -- Divisible by 3, 5, 15
  * Write a program that can print the numbers between 1 ~ 100 that can be divisible by 3, 5, and 15.
  * if the number can be divisible by 3, 5 and 15, then it should only be displayed in DivisibelBy15 section
  * if the number can be divisible by 3 but cannot be divisible by 15, then it should only be displayed in DivisibelBy3 section
@@ -13,4 +18,32 @@ package alik.week_2;
  * Divisible By 3 3 6 9 12 18 21 24 27 33 36 39 42 48 51 54 57 63 66 69 72 78 81 84 87 93 96 99
  */
 public class NumbersDivisibleBy_3_5_15 {
+    public static Map<String, ArrayList<Integer>> divisible(int n) {
+        final String div15key = "Divisible By 15";
+        final String div5key = "Divisible By 5";
+        final String div3key = "Divisible By 3";
+        ArrayList<Integer> div15int = new ArrayList<>();
+        ArrayList<Integer> div5int = new ArrayList<>();
+        ArrayList<Integer> div3int = new ArrayList<>();
+        LinkedHashMap<String, ArrayList<Integer>> result = new LinkedHashMap<>();
+        result.put(div15key, div15int);
+        result.put(div5key, div5int);
+        result.put(div3key, div3int);
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 15 == 0) {
+                div15int.add(i);
+            } else if (i % 5 == 0) {
+                div5int.add(i);
+            } else if (i % 3 == 0) {
+                div3int.add(i);
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(divisible(100));
+    }
 }
