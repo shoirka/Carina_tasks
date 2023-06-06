16f7c85ede081a0aa7047c7e7a487816721f4b8c
Aliy Shegidevich <shegidevich@gmail.com>
2023-06-06 18:35:38 -0400

    Week 3 added

---
+++ src/alik/week_3/NumbersPrimeNumber.java
@@ -0,0 +1,8 @@
+package alik.week_3;
+
+/**
+ *  Numbers -- PrimeNumber
+ * Write a method that can check if a number is prime or not
+ */
+public class NumbersPrimeNumber {
+}
---
+++ src/alik/week_3/Numbers_Reverse_Negative_Number.java
@@ -0,0 +1,4 @@
+package alik.week_3;
+
+public class Numbers_Reverse_Negative_Number {
+}
