e0a26fa431e2143231aaef18aa4f9d17f6f4d690
Aliy Shegidevich <shegidevich@gmail.com>
2023-06-06 20:52:59 -0400

    Week 3 fix

--- src/alik/week_3/Numbers_Reverse_Negative_Number.java
+++ src/alik/week_3/Numbers_Reverse_Negative_Number.java
@@ -3,13 +3,13 @@ package alik.week_3;
 public class Numbers_Reverse_Negative_Number {
 
     public static int reverse(int number) {
-        if (number==0) {
-            return 0;
+        if (number>=0) {
+            return number;
         }
 
         String strNumber = String.valueOf(number);
         StringBuilder reversedStrNumber = new StringBuilder("-");
-        for (int i = strNumber.length() - 1; i >= 0; i--) {
+        for (int i = strNumber.length() - 1; i >= 1; i--) {
             reversedStrNumber.append(strNumber.charAt(i));
         }
         return Integer.parseInt(reversedStrNumber.toString());
@@ -17,6 +17,7 @@ public class Numbers_Reverse_Negative_Number {
 
     public static void main(String[] args) {
         System.out.println(reverse(567));
+        System.out.println(reverse(-567));
     }
 
 }
