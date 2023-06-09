453b0eb695aadeca50830b3d13a94b66ac48b592
Aliy Shegidevich <shegidevich@gmail.com>
2023-06-09 00:15:35 -0400

    Week 4 added

---
+++ src/alik/week_4/StringFrequencyOfCharacters.java
@@ -0,0 +1,9 @@
+package alik.week_4;
+
+/**
+ *   String - Frequency of Characters
+ * Write a return method that can find the frequency of characters
+ *   Ex: FrequencyOfChars("AAABBCDD") ==> A3B2C1D2
+ */
+public class StringFrequencyOfCharacters {
+}
---
+++ src/alik/week_4/StringRemoveDuplicates.java
@@ -0,0 +1,9 @@
+package alik.week_4;
+
+/**
+ *   String - Remove Duplicates
+ * Write a return method that can remove the duplicated values from the String
+ *   Ex: removeDup("AAABBBCCC") ==> ABC
+ */
+public class StringRemoveDuplicates {
+}
