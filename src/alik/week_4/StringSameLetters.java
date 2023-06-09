b9f3dffd7e048aff1a45bb683eaa786321fc4c33
Aliy Shegidevich <shegidevich@gmail.com>
2023-06-09 00:46:58 -0400

    Week 4 solutions

--- src/alik/week_4/StringSameLetters.java
+++ src/alik/week_4/StringSameLetters.java
@@ -1,5 +1,7 @@
 package alik.week_4;
 
+import java.util.Arrays;
+
 /**
  *   String - Same letters
  * Write a return method that checks if a string is built out of the same letters as another string.
@@ -7,4 +9,17 @@ package alik.week_4;
  *       same("abc", "abb"); ==> false
  */
 public class StringSameLetters {
+    public static boolean same(String str1, String str2) {
+        char[] arr1 = str1.toCharArray();
+        char[] arr2 = str2.toCharArray();
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        return Arrays.equals(arr1, arr2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(same("abc", "cab"));
+        System.out.println(same("abc", "abb"));
+    }
 }
