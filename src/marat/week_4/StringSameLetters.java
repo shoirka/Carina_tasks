43cbb85ddd584301496595edf3807fc48adec17f
marat-aksiev <aksievmarat@gmail.com>
2023-06-09 10:37:33 -0400

    Week 4 solutions

---
+++ src/marat/week_4/StringSameLetters.java
@@ -0,0 +1,12 @@
+package marat.week_4;
+
+/**
+ *   String - Same letters
+ * Write a return method that checks if a string is built out of the same letters as another string.
+ *   Ex: same("abc", "cab"); ==> true
+ *       same("abc", "abb"); ==> false
+ */
+public class StringSameLetters {
+
+
+}
