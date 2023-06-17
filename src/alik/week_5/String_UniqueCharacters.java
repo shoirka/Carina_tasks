b9de39041d10fdc83e2e93f3511d5e9880ff1944
Aliy Shegidevich <shegidevich@gmail.com>
2023-06-16 22:03:37 -0400

    Week 5 added

---
+++ src/alik/week_5/String_UniqueCharacters.java
@@ -0,0 +1,9 @@
+package alik.week_5;
+/**
+ *   String_UniqueCharacters
+ * Write a return  method that can find the unique characters from the String
+ * Ex:  unique("AAABBBCCCDEF")  ==>  "DEF";
+ */
+public class String_UniqueCharacters {
+
+}
