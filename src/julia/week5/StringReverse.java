1b8af3183700b53e09b1bd1d2b8786ac60263294
julialinkola <julia.belenko@gmail.com>
2023-06-21 22:17:08 -0400

    week5 reverse string task created

--- src/julia/week5/StringReverse.java
+++ src/julia/week5/StringReverse.java
@@ -1,4 +1,23 @@
 package julia.week5;
 
 public class StringReverse {
+    public static String reverse(String string) {
+        String reverse = "";
+
+        for (int i = string.length() - 1; i >= 0; i--) {
+            reverse += string.charAt(i);
+        }
+
+        return reverse;
+
+    }
+
+    public static void main(String[] args) {
+
+        // System.out.println("reverse(\"java\") = " + reverse("java"));
+        // System.out.println("reverse(\"palindrome\") = " + reverse("palindrome"));
+        System.out.println("reverse(\"massachusetts\") = " + reverse("massachusetts"));
+
+    }
 }
+
