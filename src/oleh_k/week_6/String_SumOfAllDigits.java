62fda85c3eecc2e3c3ae5adf1a3026752183b368
Oleh_K <oleh.usa2017@gmail.com>
2023-06-23 13:08:44 -0400

    Week_5 solutions

--- src/oleh_k/week_6/String_SumOfAllDigits.java
+++ src/oleh_k/week_6/String_SumOfAllDigits.java
@@ -27,10 +27,11 @@ public class String_SumOfAllDigits {
                 "0 0 0 word 0",                   // Sum: 0
                 "9876543 apple 21 banana",        // Sum: 9876564
                 "word 1",                         // Sum: 1
-                "12 apple 34 banana 56 78 word",  // Sum: 181
+                "12 apple 34 banana 56 78 word",  // Sum: 180
                 "-100 apple 200 -300 banana 400", // Sum: 200
                 "-1 banana -2 apple -3 word -4",  // Sum: -10
-                "apple 5 java -3 banana word"     // Sum: 2
+                "apple 5 java -3 banana word",   // Sum: 2
+                "banana" // Sum: 0
         };
         for (String each : testCases) {
             System.out.println(each+" -> "+sumOfDigits(each));
